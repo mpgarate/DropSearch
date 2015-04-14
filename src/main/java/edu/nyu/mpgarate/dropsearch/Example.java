143c640c9ca2a55eb91a3df5e8dc374ebe904d0c
Michael Garate <mpgarate@gmail.com>
2015-04-14 16:54:31 -0400

    initialize starter java gradle project

---
+++ src/main/java/edu/nyu/mpgarate/dropsearch/Example.java
@@ -0,0 +1,10 @@
+package edu.nyu.mpgarate.dropsearch;
+
+/**
+ * Created by mike on 4/14/15.
+ */
+public class Example {
+    public String helloWorld(){
+        return "hello world";
+    }
+}
---
+++ src/test/java/edu/nyu/mpgarate/dropsearch/ExampleTest.java
@@ -0,0 +1,17 @@
+package edu.nyu.mpgarate.dropsearch;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ * Created by mike on 4/14/15.
+ */
+public class ExampleTest {
+    @Test
+    public void helloWorldTest(){
+        Example e = new Example();
+        assertEquals("hello world", e.helloWorld());
+    }
+
+}
