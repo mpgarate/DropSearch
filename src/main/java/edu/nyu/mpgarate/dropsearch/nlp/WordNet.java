e6a612ab4c4ac6efd2377395a96d272afd3c97fe
Michael Garate <mpgarate@gmail.com>
2015-05-03 17:29:29 -0400

    add small stemming example

--- src/main/java/edu/nyu/mpgarate/dropsearch/nlp/WordNet.java
+++ src/main/java/edu/nyu/mpgarate/dropsearch/nlp/WordNet.java
@@ -1,5 +1,6 @@
 package edu.nyu.mpgarate.dropsearch.nlp;
 
+import rita.RiTa;
 import rita.RiWordNet;
 
 import java.util.*;
@@ -15,27 +16,73 @@ public class WordNet {
     private final static Logger LOGGER = Logger.getLogger(WordNet.class.getName
             ());
 
+    private final static String[] PARTS_OF_SPEECH = new String[] {"n", "v",
+            "r", "a"};
+
     /**
      * From a single word, produce a list of any stems found that are not the
      * same as the input term.
+     *
+     * First, an attempt is made using the Pling algorithm
+     * https://github.com/dhowe/RiTa/blob/b4c753a32d2db1cb22e927cfb0e0a11882dbfec1/src/rita/support/PlingStemmer.java
+     *
+     * If this fails, then WordNet is used as a backup.
      *
      * @param term
      * @return any unique stems
      */
     public static List<String> getAnyStems(String term){
+
+
         Set<String> stems = new HashSet<>();
         stems.add(term);
 
-        for (String pos : new String[] {"n", "v", "r", "a"}){
-            stems.addAll(Arrays.asList(riWordNet.getStems(term, pos)));
+        // first try using the PLING algorithm
+        stems.addAll(Arrays.asList(RiTa.stem(term, RiTa.PLING)));
 
-            for (String stem : stems){
-                LOGGER.info(stem);
+        // if none found, use WordNet
+        if (1 == stems.size()){
+            for (String pos : PARTS_OF_SPEECH) {
+                stems.addAll(Arrays.asList(riWordNet.getStems(term, pos)));
             }
         }
+
 
         stems.remove(term);
 
         return new ArrayList<String>(stems);
+    }
+
+    public static List<String> getRelatedWords(String term){
+        Set<String> relatedWords = new HashSet<>();
+
+        for (String pos : PARTS_OF_SPEECH){
+            relatedWords.addAll(Arrays.asList(riWordNet.getAllSynsets(term,
+                    pos)));
+
+        }
+
+        if (relatedWords.isEmpty()){
+            for (String pos : PARTS_OF_SPEECH) {
+                relatedWords.addAll(Arrays.asList(riWordNet.getAllMeronyms(term,
+                                pos)));
+
+                }
+        }
+
+        List<String> relatedWordsList = new ArrayList<String>();
+
+        for (String w : relatedWords){
+            for (String pos : PARTS_OF_SPEECH) {
+                Float dist = riWordNet.getDistance(term, w,pos);
+
+                if (dist < 0.3){
+                    relatedWordsList.add(w);
+                    break;
+                }
+            }
+        }
+
+        return relatedWordsList;
     }
 }
---
+++ src/test/java/edu/nyu/mpgarate/dropsearch/nlp/WordNetTest.java
@@ -0,0 +1,85 @@
+package edu.nyu.mpgarate.dropsearch.nlp;
+
+import edu.nyu.mpgarate.dropsearch.nlp.WordNet;
+import org.junit.Test;
+import rita.RiString;
+import rita.RiTa;
+import rita.RiWordNet;
+import rita.support.Stemmer;
+import rita.wordnet.jwnl.wndata.Word;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by mike on 5/2/15.
+ */
+public class WordNetTest {
+
+    private boolean stemsContains(String term, String expectedStem){
+        return WordNet.getAnyStems(term).contains(expectedStem);
+    }
+    @Test
+    public void getAnyStemsTest() {
+        assertFalse(stemsContains("running", "running"));
+
+        assertTrue(stemsContains("running", "run"));
+        assertTrue(stemsContains("slapped", "slap"));
+        assertTrue(stemsContains("argues", "argue"));
+        assertTrue(stemsContains("cats", "cat"));
+        assertTrue(stemsContains("firemen", "fireman"));
+        assertTrue(stemsContains("appendices", "appendix"));
+        assertTrue(stemsContains("dries", "dry"));
+        assertTrue(stemsContains("physics", "physic"));
+        assertTrue(stemsContains("people", "person"));
+    }
+
+    private boolean relatedWordsContains(String term, String relatedTerm){
+        return WordNet.getRelatedWords(term).contains(relatedTerm);
+    }
+
+    @Test
+    public void getRelatedWordsTest(){
+        assertTrue(relatedWordsContains("computer", "calculator"));
+        assertTrue(relatedWordsContains("university", "college"));
+        assertTrue(relatedWordsContains("universe", "existence"));
+        assertTrue(relatedWordsContains("fish", "angle"));
+        assertTrue(relatedWordsContains("text", "schoolbook"));
+        assertTrue(relatedWordsContains("steward", "keeper"));
+    }
+
+
+//
+//        RiString rs = new RiString(term);
+//        System.out.println(rs.analyze());
+//        System.out.println(rs.features());
+//
+//        RiTa riTa = new RiTa();
+//        RiWordNet wn = WordNet.get
+//
+//        String[] syns = wn.getAllSynonyms(term, "n", 10);
+//        String[] stems = wn.getStems(term, "v");
+//
+//        for (String pos : wn.getPos(term)){
+//            System.out.println(pos);
+//        }
+//
+//        System.out.println(wn.getPos(term)[0]);
+//
+//
+//
+//        System.out.println("stems:");
+//
+//        for (String stem : stems){
+//            System.out.println(stem);
+//        }
+//
+//        System.out.println("syms:");
+//
+//        for (String syn : syns){
+//            System.out.println(syn);
+//        }
+}
